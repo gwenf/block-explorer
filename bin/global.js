db2bd3a98d3417c0628d757de7cf6c3ef3f4eefb
gwenf <gwenfaraday@gmail.com>
2018-09-01 17:13:44 -0400

    minor cleanup

--- bin/global.js
+++ bin/global.js
@@ -6,13 +6,11 @@ require('dotenv').config();
 var bexLibrary = require('../lib/index.js');
 
 program
-    .version('0.1.0')
+    .version('1.0.0')
     .option('-s, --start', 'Start Block')
     .option('-e, --end', 'End Block')
     .parse(process.argv);
 
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);
+console.log('The blockrange is:');
+if (program.start) console.log('  - start');
+if (program.end) console.log('  - end');
