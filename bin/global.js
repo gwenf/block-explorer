72e481341b7c03dfe8269ed151207a3e19f01c3e
gwenf <gwenfaraday@gmail.com>
2018-09-05 10:22:16 -0400

    working on terminal input

--- bin/global.js
+++ bin/global.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const program = require('commander');
 require('dotenv').config();
 
@@ -7,10 +8,28 @@ var bexLibrary = require('../lib/index.js');
 
 program
     .version('1.0.0')
+    .option('-i, --init', 'Create .env file')
     .option('-s, --start', 'Start Block')
     .option('-e, --end', 'End Block')
     .parse(process.argv);
 
-console.log('The blockrange is:');
-if (program.start) console.log('  - start');
-if (program.end) console.log('  - end');
+(async function main() {
+    if (program.init) {
+        fs.writeFile('.env', `API_KEY=${program.init}`, 'utf8', function (err) {
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log('Your .env file was created successfully!');
+        }); 
+    } else if (program.start) {
+        let end = program.end ? program.end : 'latest';
+        console.log(`The blockrange is: ${program.start}-${end}`);
+
+        if (end === 'latest') {
+            end = bexLibrary.fetchBlocks.getLatestBlock();
+        }
+    } else {
+        console.log('Invalid entry. Use --help flag to see full list of commands.')
+    }
+})()
