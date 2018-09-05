cc03f501eb15581565ec8c9835f4dbf546388caa
gwenf <gwenfaraday@gmail.com>
2018-09-05 10:29:51 -0400

    added functionality to pass variables through command line

--- bin/global.js
+++ bin/global.js
@@ -6,11 +6,12 @@ require('dotenv').config();
 
 var bexLibrary = require('../lib/index.js');
 
+// TODO: need to pass variables in the command line
 program
     .version('1.0.0')
     .option('-i, --init', 'Create .env file')
-    .option('-s, --start', 'Start Block')
-    .option('-e, --end', 'End Block')
+    .option('-s, --start [startBlock]', 'Start Block')
+    .option('-e, --end [endBlock]', 'End Block')
     .parse(process.argv);
 
 (async function main() {
@@ -27,8 +28,10 @@ program
         console.log(`The blockrange is: ${program.start}-${end}`);
 
         if (end === 'latest') {
-            end = bexLibrary.fetchBlocks.getLatestBlock();
+            end = await bexLibrary.fetchBlocks.getLatestBlock();
         }
+
+        console.log(end);
     } else {
         console.log('Invalid entry. Use --help flag to see full list of commands.')
     }
