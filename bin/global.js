36414919be82ed9b5684a4408b3e0ede3f5c19c7
gwenf <gwenfaraday@gmail.com>
2018-09-05 11:04:54 -0400

    updating api functions and code cleanup

--- bin/global.js
+++ bin/global.js
@@ -2,16 +2,18 @@
 
 const fs = require('fs');
 const program = require('commander');
+const web3 = require('web3');
 require('dotenv').config();
 
 var bexLibrary = require('../lib/index.js');
 
-// TODO: need to pass variables in the command line
 program
     .version('1.0.0')
-    .option('-i, --init', 'Create .env file')
-    .option('-s, --start [startBlock]', 'Start Block')
-    .option('-e, --end [endBlock]', 'End Block')
+    .option('-i, --init [apiKey]', 'Create .env file')
+    .option('-s, --start [startBlock]', 'Denote integer value of start block')
+    .option('-e, --end [endBlock]', 'Denote integer value of end block')
+    .option('-c, --contract', 'Include contract address list')
+    .option('-l, --latest', 'Get number of latest block')
     .parse(process.argv);
 
 (async function main() {
@@ -24,14 +26,30 @@ program
             console.log('Your .env file was created successfully!');
         }); 
     } else if (program.start) {
-        let end = program.end ? program.end : 'latest';
-        console.log(`The blockrange is: ${program.start}-${end}`);
+        const start = parseInt(web3.utils.hexToNumberString(program.start), 10);
+        let end = program.end;
 
-        if (end === 'latest') {
+        if (!end) {
             end = await bexLibrary.fetchBlocks.getLatestBlock();
         }
+        end = parseInt(web3.utils.hexToNumberString(end), 10);
 
-        console.log(end);
+        console.log(`The blockrange is: ${start}-${end}`);
+
+        if (end < start) {
+            console.log('Ending block value must be greater than start.');
+            return false;
+        }
+        // TODO: limit the amount of blocks to be processed
+        // TODO: figure out how to be more memory efficient
+
+        console.log('Processing... This may take a minute.');
+        bexLibrary.getData(program.start, end, program.contract);
+    } else if (program.latest) {
+        const latest = await bexLibrary.fetchBlocks.getLatestBlock();
+        const latestInt = parseInt(web3.utils.hexToNumberString(latest), 10);
+
+        console.log('This is the latest Ethereum block mined:', `${latest} (hex)`, `${latestInt} (int)`);
     } else {
         console.log('Invalid entry. Use --help flag to see full list of commands.')
     }
