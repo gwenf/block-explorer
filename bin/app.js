8ac7367428a8b91d42d8f41f33adad699b7a4e7b
gwenf <gwenfaraday@gmail.com>
2018-09-30 08:57:56 -0400

    partial implementation of block ranges from terminal

--- bin/app.js
+++ bin/app.js
@@ -1,36 +1,39 @@
 const web3 = require('web3');
 const chalk = require('chalk');
 const Table = require('cli-table'); // TODO: create tables to display information
-require('dotenv').config();
+// ^^ also can use the blessed library for tables: https://github.com/chjj/blessed#table-from-box
 
 var bexLibrary = require('../lib/index.js');
 
 // TODO: should check if it's not a valid api key and let user know to check it
 async function main(answer) {
-    console.log(answer);
-    if (answer.action === 'range') {
-        const start = parseInt(web3.utils.hexToNumberString(answer), 10);
+    const { action } = answer.action;
+    if (action === 'range') {
+        const { start, end } = answer.range;
+        const start = parseInt(web3.utils.hexToNumberString(start), 10);
         let end = program.end;
 
-        if (!end) {
+        if (end === 'latest') {
             end = await bexLibrary.fetchBlocks.getLatestBlock();
         }
         end = parseInt(web3.utils.hexToNumberString(end), 10);
+        console.log(end, start);
 
-        if (end < start) {
-            console.log(chalk.red('Ending block value must be greater than start.'));
-            return false;
-        }
+        // if (end < start) {
+        //     console.log(chalk.red('Ending block value must be greater than start.'));
+        //     return false;
+        // }
         // TODO: limit the amount of blocks to be processed?
         // TODO: figure out how to be more memory efficient when more blocks are processed
 
         console.log(chalk.yellow('Processing... This may take a minute.'));
-        const blockData = await bexLibrary.getData(program.start, end, program.contract);
-        return {
-            blockData,
-            end,
-            start
-        };
+        // const blockData = await bexLibrary.getData(program.start, end, program.contract);
+        
+        // TODO: this should be in table format:
+        // console.log(chalk.blue('The blockrange is:'), `${data.start}-${data.end}`);
+        // console.log(chalk.blue('The total Ether is:'), data.blockData.totalEther);
+        // console.log(chalk.magenta('Sending addresses:'), data.blockData.sendingAddresses);
+        // console.log(chalk.magenta('Receiving addresses:'), data.blockData.receivingAddresses);
     } else if (answer.action === 'latest') {
         const latest = await bexLibrary.fetchBlocks.getLatestBlock();
         const latestInt = parseInt(web3.utils.hexToNumberString(latest), 10);
--- bin/global.js
+++ bin/global.js
@@ -5,6 +5,7 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const inquirer = require('inquirer');
+require('dotenv').config();
 
 const bexApp = require('./app');
 var bexLibrary = require('../lib/index.js');
@@ -33,6 +34,7 @@ program
 
         (async function chooseAction() {
 
+            // TODO: make a separate file for inquirer prompts
             const answers = await inquirer.prompt({
                 type : 'list',
                 name : 'action',
@@ -48,16 +50,37 @@ program
                     }
                 ]
             })
-            
-            await bexApp(answers);
 
-            if (answers.action !== 'Exit') {
-                console.log(chalk.white('----------------------------'));
-                return chooseAction();
-            } else {
+            if (answers.action === 'Exit') {
                 console.log(chalk.white('Have a nice day!'));
                 return ;
             }
+
+            if (answers.action === 'latest') {
+                await bexApp(answers);
+            } else if (answers.action === 'range') {
+                // TODO: add validation here for valid block numbers + end must be greater than start or blank
+                const questions = [
+                    {
+                        type: 'input',
+                        name: 'start',
+                        message: 'Enter a start block:'
+                    },
+                    {
+                        type: 'input',
+                        name: 'end',
+                        default: 'latest',
+                        message: 'Enter a end block (leave blank for latest block mined):'
+                    }
+                ];
+                const range = await inquirer.prompt(questions);
+                console.log(range);
+
+                await bexApp({ action: answers.action, range });
+            }
+
+            console.log(chalk.white('----------------------------'));
+            return chooseAction();
         })()
     });
 
@@ -71,6 +94,7 @@ function startLogs() {
     console.log('');
 }
 
+// OLD API REFERENCE TO BE REMOVED WHEN THERE IS PARITY BETWEEN VERSIONS 1 AND 2:
 // program
 //     .version('1.2.1')
 //     .option('-i, --init [apiKey]', 'Create .env file')
@@ -79,25 +103,3 @@ function startLogs() {
 //     .option('-c, --contract', 'Boolean for including contract address list in results')
 //     .option('-l, --latest', 'Get number of latest block')
 //     .parse(process.argv);
-
-(async function main() {
-    // if (program.init) {
-    //     return ;
-    // }
-
-    // clear();
-
-    // console.log(chalk.yellow(figlet.textSync('BXE', { horizontalLayout: 'full' })));
-    // console.log(chalk.white('Ethereum Blockchain Explorer'));
-    
-    // if (program.start) {
-    //     console.log(chalk.blue('The blockrange is:'), `${data.start}-${data.end}`);
-    //     console.log(chalk.blue('The total Ether is:'), data.blockData.totalEther);
-    //     console.log(chalk.magenta('Sending addresses:'), data.blockData.sendingAddresses);
-    //     console.log(chalk.magenta('Receiving addresses:'), data.blockData.receivingAddresses);
-    // } else if (program.latest) {
-    //     console.log(chalk.blue('This is the latest Ethereum block mined:'), `${chalk.underline(data.latest)} (hex)`, `${chalk.underline(data.latestInt)} (int)`);
-    // } else {
-    //     console.log(chalk.red('Invalid entry. Use --help flag to see full list of commands.'));
-    // }
-})()
