b6f8ddb26032ace9b42bf33c27c429ab6cfaa3be
gwenf <gwenfaraday@gmail.com>
2018-10-17 15:30:44 -0400

    fixing errors with contract output

--- bin/app.js
+++ bin/app.js
@@ -7,32 +7,30 @@ var bexLibrary = require('../lib/index.js');
 
 // TODO: should check if it's not a valid api key and let user know to check it
 async function main(answer) {
-    const { action } = answer.action;
-    if (action === 'range') {
+    if (answer.action === 'range') {
         let { start, end } = answer.range;
-        // start = parseInt(web3.utils.hexToNumberString(start), 10);
+        // this is temporary until the api allows for contracts again:
+        let contract = null;
 
         if (end === 'latest') {
             end = await bexLibrary.fetchBlocks.getLatestBlock();
         }
-        // end = parseInt(web3.utils.hexToNumberString(end), 10);
-        console.log(end, start);
 
-        // if (end < start) {
-        //     console.log(chalk.red('Ending block value must be greater than start.'));
-        //     return false;
-        // }
+        if (end < start) {
+            console.log(chalk.red('Ending block value must be greater than start.'));
+            return false;
+        }
         // TODO: limit the amount of blocks to be processed?
         // TODO: figure out how to be more memory efficient when more blocks are processed
 
         console.log(chalk.yellow('Processing... This may take a minute.'));
-        // const blockData = await bexLibrary.getData(program.start, end, program.contract);
-        
+        const blockData = await bexLibrary.getData(start, end, contract);
+
         // TODO: this should be in table format:
-        // console.log(chalk.blue('The blockrange is:'), `${data.start}-${data.end}`);
-        // console.log(chalk.blue('The total Ether is:'), data.blockData.totalEther);
-        // console.log(chalk.magenta('Sending addresses:'), data.blockData.sendingAddresses);
-        // console.log(chalk.magenta('Receiving addresses:'), data.blockData.receivingAddresses);
+        console.log(chalk.blue('The blockrange is:'), `${start}-${end}`);
+        console.log(chalk.blue('The total Ether is:'), blockData.totalEther);
+        console.log(chalk.magenta('Sending addresses:'), blockData.sendingAddresses);
+        console.log(chalk.magenta('Receiving addresses:'), blockData.receivingAddresses);
     } else if (answer.action === 'latest') {
         const latest = await bexLibrary.fetchBlocks.getLatestBlock();
         const latestInt = parseInt(web3.utils.hexToNumberString(latest), 10);
